dcdc6a5a6e6907f88863459dea1e424804037a9d
crisiticmei <meifen20160710@gmail.com>
2018-03-13 17:33:17 +0800

    add tools

--- startDemo/codeUtils.js
+++ startDemo/codeUtils.js
@@ -1,3 +1,11 @@
+/**
+ * @file: codeUtils.js
+ * @author: fisco-dev
+ * 
+ * @date: 2017
+ */
+
+
 /*
 编码工具类，包含交易编码及日志编码
 */
@@ -10,40 +18,49 @@ var Coder = require('web3/lib/solidity/coder');
 #交易编码
 ###参数编码
 */
-function codeParams(types, params) {
-    var code_ret = Coder.encodeParams(types, params);
-    //console.log("code_ret : ",code_ret);
-    return code_ret;
+function codeParams(types,params)
+{
+	var code_ret = Coder.encodeParams(types, params);
+	//console.log("code_ret : ",code_ret);
+	return code_ret;
 }
 
+function decodeParams(types,bytes)
+{
+	var code_ret = Coder.decodeParams(types, bytes);
+	//console.log("code_ret : ",code_ret);
+	return code_ret;
+}
 /*
 ###函数名编码
 */
-function codeFun(fun_str) {
+function codeFun(fun_str)
+{
 
-    var code_fun = '0x' + sha3(fun_str).slice(0, 8);
-    console.log("code_fun : ", code_fun);
-    return code_fun;
+	var code_fun = '0x' + sha3(fun_str).slice(0, 8);
+	//console.log("code_fun : ",code_fun);
+	return code_fun;
 }
 
 /*
 ###交易数据编码
 */
-function codeTxData(fun_Str, types, params) {
-    var txData_code = codeFun(fun_Str);
-    txData_code += codeParams(types, params);
-    console.log("txData_code : ", txData_code);
-    return txData_code;
+function codeTxData(fun_Str,types,params)
+{
+	var txData_code = codeFun(fun_Str);
+	txData_code += codeParams(types,params);
+	//console.log("txData_code : ",txData_code);
+	return txData_code;
 }
 
-function hex2a(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) {
-        var v = parseInt(hex.substr(i, 2), 16);
-        if (v) str += String.fromCharCode(v);
-    }
-    return str;
-}
+/*
+var test_fun="SetAuth(string,string)";
+var types=['string','string'];
+var params=['123','abc'];
+codeTxData(test_fun,types,params);
+*/
 
-exports.codeTxData = codeTxData;
-exports.hex2a = hex2a;
+
+exports.codeTxData=codeTxData;
+exports.codeParams=codeParams;
+exports.decodeParams=decodeParams;
--- startDemo/config.js
+++ startDemo/config.js
@@ -1,4 +1,6 @@
-var proxy="http://127.0.0.1:8545";
+
+var proxy="http://127.0.0.1:8545";  // <http://ip:port> 这里请填写发送rpc请求的区块链的ip及端口
+
 console.log('RPC='+proxy);
 var output="./output/";
 console.log('Ouputpath='+output);
@@ -6,6 +8,6 @@ console.log('Ouputpath='+output);
 module.exports={
 	HttpProvider:proxy,
 	Ouputpath:output,
-	privKey:"6227ce272f414fbeab5bd9ba514a8b7ba68d5f292d22f2ed8512d3ae837b28c1",
-	account:"0x3ff2f8149f12ebcdae1b04ba5cd16cec2d31ddbb"
+	privKey:"bcec428d5205abe0f0cc8a734083908d9eb8563e31f943d760786edf42ad67dd",
+	account:"0x64fa644d2a694681bd6addd6c5e36cccd8dcdde3"
 }
